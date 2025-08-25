797b0f21370ced086c578081a8c329da94371b08
Niamh Driscoll <niamhmolson@outlook.com>
2025-08-24 19:34:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Smaker {
+    public void Unity() {
+        Process.Start("unity.ps1");
+    }
+
+}
+class Program {
+    static void Main(string[] args) {
+        Smaker smaker = new Smaker();
+        smaker.Unity();
+    }
+}
---
+++ unity.ps1
@@ -0,0 +1,3 @@
+//$X = "C:\Program Files\Unity\Hub\Editor\<version>\Editor\Unity.exe" -projectPath "<project path>"
+//mkdir $X
+C:\Program Files\Unity\Hub\Editor\6000.0.23f1\Editor\Unity.exe" -projectPath "C:\Users\niamh_8ref0it\My project"
